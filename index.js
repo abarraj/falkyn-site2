636101b8f1ec23eb8749c14186ca4458dafd73e7
abarraj <andrew@barraj.net>
2025-05-12 12:39:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#004225', color: 'white', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Welcome to Falkyn</h1>
+    </div>
+  );
+}
